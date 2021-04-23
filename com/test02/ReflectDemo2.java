8666e4f5d1bedcb8e61b40c594b93d5ce32c696c
lovixk <991807272@qq.com>
2021-04-23 09:16:25 +0800

    update

---
+++ com/test02/ReflectDemo2.java
@@ -0,0 +1,37 @@
+package com.test02;
+
+import com.sun.corba.se.impl.orbutil.concurrent.Sync;
+
+import java.lang.reflect.Constructor;
+
+public class ReflectDemo2 {
+    public static void main(String arg[]) throws ClassNotFoundException, NoSuchMethodException {
+        Class<?> c = Class.forName("com.test01.Student");
+        // 通过反射获取当前类的所有构造方法
+        Constructor<?>[] cons = c.getConstructors();
+        for (Constructor con : cons) {
+            System.out.println(con);
+        }
+
+        System.out.println("----------");
+
+        // 获取当前无参构造方法
+        Constructor<?> c2 = c.getConstructor();
+        System.out.println(c2);
+
+        //         获取当前有参构造方法
+        Constructor<?> c3 = c.getConstructor(String.class, int.class, String.class);
+        System.out.println(c3);
+        //         获取当前私有构造方法
+        Constructor<?>[] c4 = c.getDeclaredConstructors();
+        for (Constructor con : c4) {
+            System.out.println(con);
+        }
+        System.out.println("---------");
+
+        // 获取当前单个私有构造方法
+
+        Constructor<?> c5 = c.getDeclaredConstructor(String.class);
+        System.out.println(c5);
+    }
+}
