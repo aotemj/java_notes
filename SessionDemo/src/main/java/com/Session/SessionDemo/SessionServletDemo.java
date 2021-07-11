e0ba034592c064ccf33aee7c7d871e76767d6a4b
losidk <991807272@qq.com>
2021-07-11 22:16:37 +0800

    持久化 session

--- SessionDemo/src/main/java/com/Session/SessionDemo/SessionServletDemo.java
+++ SessionDemo/src/main/java/com/Session/SessionDemo/SessionServletDemo.java
@@ -15,6 +15,13 @@ public class SessionServletDemo extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession httpSession = request.getSession();
+        System.out.println("httpSession: "+httpSession);
+//         使用cookie 保存 session ，解决服务器不关闭，客户端关闭重启后的session 不一样问题
+        Cookie cookie = new Cookie("JSESSIONID",httpSession.getId());
+//         设置超时时间
+        cookie.setMaxAge(60*60);
+//
+        response.addCookie(cookie);
         System.out.println("request invoke");
         httpSession.setAttribute("msg","hello1");
 
