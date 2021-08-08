0e258ecd637030febc46e8ef9ff7659e1035994d
losidk <991807272@qq.com>
2021-08-08 08:26:59 +0800

    filter 快速入门

---
+++ Filter/src/main/java/com/filter/Filter/FilterDemo.java
@@ -0,0 +1,25 @@
+package com.filter.Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*") // *: 通配符, 表示所有的资源在被访问的时候都会经过当前过滤器
+public class FilterDemo implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("filter 被执行了");
+//         放行操作
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
